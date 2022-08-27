07d300063935a5d8d60089f6d3eac6730f89aaf9
SaberHardy <samirsamado5@gmail.com>
2022-08-27 12:44:59 +0100

    serialization task model

--- todoAjaxApp/urls.py
+++ todoAjaxApp/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("list/", views.task_list, name="task_list"),
+    path("detail/<str:pk>/", views.task_detail, name="task_detail"),
+    path("create/", views.task_create, name="task_create"),
+    path("update/<str:pk>/", views.task_update, name="task_update"),
+    path("delete/<str:pk>/", views.task_delete, name="task_delete"),
 ]
--- todoAjaxApp/views.py
+++ todoAjaxApp/views.py
@@ -1,7 +1,11 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import Task
+from .serialzers import TaskSerializer
 
 
 @api_view(['GET'])
@@ -14,3 +18,46 @@ def home(request):
         'Delete': '/delete/<str:pk>/',
     }
     return Response(api_url)
+
+
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def task_detail(request, pk):
+    tasks = Task.objects.get(pk=pk)
+    serializer = TaskSerializer(tasks, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def task_create(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def task_update(request, pk):
+    task = Task.objects.get(pk=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def task_delete(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.delete()
+
+    return Response("Task Api Successfully deleted!!!")
